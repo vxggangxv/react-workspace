d278fd2daf1811f8dfba9290b875aeaca266e561
ryanBuzzni <102950109+ryanBuzzni@users.noreply.github.com>
2024-04-21 03:04:35 +0900

    feat: test 2

--- src/App.js
+++ src/App.js
@@ -3,38 +3,42 @@ import {
   QueryClientProvider,
   useQuery,
 } from "@tanstack/react-query";
-import { useState } from "react";
+import { memo, useEffect, useState } from "react";
 import "./App.css";
 
 const fetchTodo = (id) => {
   console.log("fetchTodo call");
+  const randomId = Math.floor(Math.random() * 5) + 1;
+  console.log("randomId", randomId);
 
-  return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`).then(
-    (response) => response.json()
-  );
+  return fetch(
+    `https://jsonplaceholder.typicode.com/todos/${id || randomId}`
+  ).then((response) => response.json());
 };
 
 const queryClient = new QueryClient();
 
-function TodoItem({ id }) {
+const TodoItem = memo(({ id }) => {
   const { data } = useQuery({
-    queryKey: ["todo", id],
+    queryKey: ["todo"],
     // queryFn: fetchTodo,
     staleTime: Infinity,
   });
 
-  console.log("TodoItem data", data);
+  useEffect(() => {
+    console.log("TodoItem data", data);
+  }, [data]);
 
   return <div>TodoItem</div>;
-}
+});
 
 function App() {
   const [shown, setShown] = useState(false);
   const [id, setId] = useState(0);
-  const { data } = useQuery({
-    queryKey: ["todo", id],
-    queryFn: () => fetchTodo(id),
-    enabled: !!id,
+  const { data, refetch } = useQuery({
+    queryKey: ["todo"],
+    queryFn: () => fetchTodo(),
+    // enabled: !!id,
   });
 
   // console.log("App data", data);
@@ -43,8 +47,9 @@ function App() {
   return (
     <div>
       <button onClick={() => setShown((prev) => !prev)}>Todo 토글</button>
-      <button onClick={() => setId((prev) => ++prev)}>Id 추가</button>
-      {shown && <TodoItem id={id} />}
+      {/* <button onClick={() => setId((prev) => ++prev)}>Id 추가</button> */}
+      <button onClick={refetch}>리패치</button>
+      {shown && <TodoItem />}
     </div>
   );
 }
