604d2920a956fac945fac034ce1be5556ac8185d
ryanBuzzni <102950109+ryanBuzzni@users.noreply.github.com>
2024-04-27 01:43:37 +0900

    feat: testing react-query

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import {
   QueryClient,
   QueryClientProvider,
+  queryOptions,
   useQuery,
 } from "@tanstack/react-query";
 import { memo, useEffect, useState } from "react";
 import "./App.css";
 
+const queryClient = new QueryClient();
+
 const fetchTodo = (id) => {
-  console.log("fetchTodo call");
-  const randomId = Math.floor(Math.random() * 5) + 1;
-  console.log("randomId", randomId);
+  console.log(`fetchTodo call id: ${id}`);
+  // const randomId = Math.floor(Math.random() * 5) + 1;
+  // console.log("randomId", randomId);
 
   return fetch(
-    `https://jsonplaceholder.typicode.com/todos/${id || randomId}`
-  ).then((response) => response.json());
-};
+    `https://jsonplaceholder.typicode.com/todos/${
+      id || Math.floor(Math.random() * 5) + 1
+    }`
+  ).then((response) => {
+    console.log("response");
 
-const queryClient = new QueryClient();
+    return response.json();
+  });
+};
 
 const TodoItem = memo(({ id }) => {
   const { data } = useQuery({
@@ -32,12 +39,41 @@ const TodoItem = memo(({ id }) => {
   return <div>TodoItem</div>;
 });
 
+function groupOptions(id) {
+  return queryOptions({
+    queryKey: ["groups", id],
+    // queryFn: () => fetchTodo(id),
+    // staleTime: 1000,
+  });
+}
+
+const Todo1 = memo(() => {
+  const { data } = useQuery(groupOptions(1));
+  return <p>{data?.id}</p>;
+});
+
+const Todo2 = memo(() => {
+  // const { data } = useQuery(groupOptions(2));
+  // const data = {}
+  const { data } = useQuery({ queryKey: ["todo"], staleTime: Infinity });
+  console.log("data", data);
+  // const data = queryClient.getQueryData(["todo"]);
+  // console.log("data", data);
+  // console.log("queryClient", queryClient.getQueryData(["todo"]));
+
+  // const queryCache = queryClient.getQueryCache();
+  // console.log("queryCache", queryCache);
+
+  // return "";
+  return <p>{data?.id}</p>;
+});
+
 function App() {
   const [shown, setShown] = useState(false);
-  const [id, setId] = useState(0);
-  const { data, refetch } = useQuery({
+  const [id, setId] = useState(1);
+  const { data, refetch, isSuccess } = useQuery({
     queryKey: ["todo"],
-    queryFn: () => fetchTodo(),
+    queryFn: () => fetchTodo(id),
     // enabled: !!id,
   });
 
@@ -46,10 +82,23 @@ function App() {
 
   return (
     <div>
-      <button onClick={() => setShown((prev) => !prev)}>Todo 토글</button>
+      {/* <button onClick={() => setShown((prev) => !prev)}>Todo 토글</button> */}
+      <button
+        onClick={() => {
+          setId((prev) => ++prev);
+          setTimeout(() => {
+            refetch();
+          }, 1000);
+        }}
+      >
+        갱신 리패치
+      </button>
       {/* <button onClick={() => setId((prev) => ++prev)}>Id 추가</button> */}
       <button onClick={refetch}>리패치</button>
-      {shown && <TodoItem />}
+      <p>App 데이타 {data?.id}</p>
+      {/* {shown && <TodoItem />} */}
+      {/* <Todo1 /> */}
+      {isSuccess && <Todo2 />}
     </div>
   );
 }
